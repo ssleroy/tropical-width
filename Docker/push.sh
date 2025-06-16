3a9e324094d4256ca419e888536842c2a079f582
Leroy, Stephen <sleroy@aer.com>
2025-06-16 15:40:23 -0400

    cleanout

--- Docker/push.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash 
-
-aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 939314430087.dkr.ecr.us-east-1.amazonaws.com
-docker push 939314430087.dkr.ecr.us-east-1.amazonaws.com/rd-tropical-width:latest
